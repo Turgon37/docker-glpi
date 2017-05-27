6fd960aea969fc3b7ceb24e758c6ab2730fa6c27
Pierre GINDRAUD <pgindraud@gmail.com>
2017-05-28 00:05:54 +0200

    Add plugins managment

---
+++ root/start.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -e
+
+basedir="${GLPI_ROOT}"
+
+# used to remove the installer after first installation
+if [ "$GLPI_REMOVE_INSTALLER" = 'yes' ]; then
+  rm -f "${basedir}/install/install.php"
+fi
+
+plugindir="$basedir/plugins"
+echo "Installing plugins..."
+cd "$plugindir"
+for i in ${GLPI_PLUGINS}; do
+  plugin="${i%|*}"
+  url="${i#*|}"
+  file=$(basename "$url")
+  
+  # continue if plugin already installed
+  if [ -d "$plugin" ]; then
+    echo "..plugin $plugin already installed"
+    continue
+  fi
+  # Download plugin source if not exists
+  if [ ! -f "$file" ]; then
+    echo "..downloading plugin '$plugin' from '$url'"
+    curl --trace-ascii -o "$file" -L "$url"
+  fi
+  
+  # extract the archive according to the extension
+  echo "..extracting plugin '$file'"
+  case $file in
+    *.tar.gz)
+      tar xzf "$file"
+      ;;
+    *)
+    echo "..#ERROR# unknown extension for $file. Please open an issue or make a PR to https://github.com/Turgon37/docker-glpi"
+      ;;
+  esac
+  if [ $? -ne 0 ]; then
+    continue
+  fi
+  rm -f $file
+  chown -R www-data:www-data "$plugin"
+  chmod -R g=rX,o=--- "$plugin"
+done
+cd -
+
+exec /usr/bin/supervisord -c /etc/supervisord.conf
