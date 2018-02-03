528abb3b327bdeb167e73cb7590e09734c801b96
Pierre GINDRAUD <pgindraud@gmail.com>
2018-02-03 15:51:02 +0100

    improve tests

--- tests.sh
+++ tests.sh
@@ -2,17 +2,36 @@
 
 ## Local settings
 build_tags_file="${PWD}/build.sh~tags"
+docker_run_options='--detach --rm'
 
 ## Settings initialization
 set -e
 
 ## Tests
 
-# Test build successful
+#1 Test build successful
 [ -f "${build_tags_file}" ]
 
 # Get main image
 image=`head --lines=1 "${build_tags_file}"`
 
-# Test if GLPI successfully installed
+#2 Test if GLPI successfully installed
 docker run --rm "${image}" test -f index.php
+
+#3 Test plugins installation with tar.bz2
+image_name=glpi_3
+docker run $docker_run_options --name "${image_name}" --env='GLPI_INSTALL_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.2%2B1.0/glpi-fusioninventory-9.2.1.0.tar.bz2' "${image}"
+docker exec "${image_name}" test -d plugins/fusioninventory
+docker stop "${image_name}"
+
+#4 Test plugins installation with tar.gz
+image_name=glpi_4
+docker run $docker_run_options --name "${image_name}" --env='GLPI_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.1%2B1.1/fusioninventory-for-glpi_9.1.1.1.tar.gz' "${image}"
+docker exec "${image_name}" test -d plugins/fusioninventory
+docker stop "${image_name}"
+
+#5 Test web access
+image_name=glpi_5
+docker run $docker_run_options --name "${image_name}" --publish 8000:80 "${image}"
+curl -v http://localhost:8000 | grep 'install/install.php'
+docker stop "${image_name}"
