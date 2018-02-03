21675fce969bf603657a66744d0826dea259f426
Pierre GINDRAUD <pgindraud@gmail.com>
2018-02-03 13:24:14 +0100

    improve build

--- build.sh
+++ build.sh
@@ -6,9 +6,17 @@ DOCKER_USERNAME="${DOCKER_USERNAME:-turgon37}"
 # image name
 DOCKER_IMAGE="${DOCKER_USERNAME}/${DOCKER_IMAGE:-glpi}"
 
+## Local settings
+build_tags_file="${PWD}/build.sh~tags"
+
+## Settings initialization
+
 # If empty version, fetch the latest from repository
 if [ -z "$GLPI_VERSION" ]; then
   GLPI_VERSION=`curl -s https://api.github.com/repos/glpi-project/glpi/releases | jq --raw-output '.[] | .tag_name' | sort --reverse | grep --max-count=1 --invert-match RC`
+  if [ -z "$DOCKER_IMAGE_TAGS" ]; then
+    DOCKER_IMAGE_TAGS="${DOCKER_IMAGE_TAGS} latest"
+  fi
 fi
 echo "-> selected GLPi version ${GLPI_VERSION}"
 
@@ -28,3 +36,14 @@ docker build --build-arg VCS_REF="${VCS_REF}" \
              --tag ${DOCKER_IMAGE}:${GLPI_VERSION} \
              --file Dockerfile \
              .
+echo "${DOCKER_IMAGE}:${GLPI_VERSION}" > ${build_tags_file}
+
+# Tag images
+for tag in $DOCKER_IMAGE_TAGS; do
+  if [ -n "$tag" ]; then
+    docker tag "${DOCKER_IMAGE}:${GLPI_VERSION}" "${DOCKER_IMAGE}:${tag}"
+    echo "${DOCKER_IMAGE}:${GLPI_VERSION}" >> ${build_tags_file}
+  fi
+done
+
+cat "${build_tags_file}"
