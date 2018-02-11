230e574a126e5cf8cf5e9db578f1857c56825db3
Pierre GINDRAUD <pgindraud@gmail.com>
2018-02-11 10:51:54 +0100

    typo

--- root/start.sh
+++ root/start.sh
@@ -79,7 +79,7 @@ cd - > /dev/null
 
 
 ## Remove installer
-echo "Removing installer if needed..."
+echo 'Removing installer if needed...'
 # used to remove the installer after first installation
 if [ "x${GLPI_REMOVE_INSTALLER}" = 'xyes' ]; then
   rm -f "${basedir}/install/install.php"
@@ -99,7 +99,7 @@ done
 
 
 ## Files permissions
-echo "Set files permissions..."
+echo 'Set files permissions...'
 # address issue https://github.com/Turgon37/docker-glpi/issues/2
 if [ "x${GLPI_CHMOD_PATHS_FILES}" = 'xyes' ]; then
   chown -R www-data:www-data "${basedir}/files"
@@ -107,5 +107,5 @@ if [ "x${GLPI_CHMOD_PATHS_FILES}" = 'xyes' ]; then
 fi
 
 ## Start
-echo "Starting up..."
+echo 'Starting up...'
 exec /usr/bin/supervisord -c /etc/supervisord.conf
