3e7e1caffe2a63085c4d70296e5e71f6db017ae8
Pierre GINDRAUD <pgindraud@gmail.com>
2018-04-02 11:29:50 +0200

    deprecate cronjob

--- root/start.sh
+++ root/start.sh
@@ -106,6 +106,18 @@ if [ "x${GLPI_CHMOD_PATHS_FILES}" = 'xyes' ]; then
   chmod -R u=rwX,g=rX,o=--- "${basedir}/files"
 fi
 
+if [ "x${GLPI_ENABLE_CRONJOB}" = 'yes' ]; then
+  /etc/supervisord.conf <<EOF
+[program:bash_cron]
+command=/bin/sh -c 'while true; do sleep 60; echo `date --rfc-2822` run cron; /usr/bin/php5 ${GLPI_PATHS_ROOT}/front/cron.php; done'
+user=www-data
+stderr_logfile = /dev/stderr
+stdout_logfile = /dev/stdout
+stderr_logfile_maxbytes = 0
+stdout_logfile_maxbytes = 0
+EOF
+fi
+
 ## Start
 echo 'Starting up...'
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+exec /usr/bin/supervisord --configuration /etc/supervisord.conf
