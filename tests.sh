f39a44cadacac94d837cd18026188dfee63ddf76
Pierre GINDRAUD <pgindraud@gmail.com>
2018-11-03 13:18:35 +0100

    Add old test into new test process

--- tests.sh
+++ tests.sh
@@ -5,6 +5,7 @@
 set -e
 
 image_building_name=`cat ${PWD}/_image_build`
+docker_run_options='--detach'
 
 
 ## Prepare
@@ -21,60 +22,64 @@ if [[ -z $(which container-structure-test 2>/dev/null) ]]; then
   fi
 fi
 
+# Download tools shim.
+if [[ ! -f _tools.sh ]]; then
+  curl -L -o ${PWD}/_tools.sh https://gist.github.com/Turgon37/2ba8685893807e3637ea3879ef9d2062/raw
+fi
+source ${PWD}/_tools.sh
+
 
 ## Test
 container-structure-test \
     test --image "${image_building_name}" --config ./tests.yml
 
 
+#2 Test plugins installation with tar.bz2
+echo '-> 2 Test plugins installation with tar.bz2'
+image_name=glpi_2
+docker run $docker_run_options --name "${image_name}" --env='GLPI_INSTALL_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.3%2B1.1/fusioninventory-9.3.1.1.tar.bz2' "${image_building_name}"
+wait_for_string_in_container_logs "${image_name}" 'Starting up...'
+if ! docker exec "${image_name}" test -d plugins/fusioninventory; then
+  docker logs "${image_name}"
+  false
+fi
+stop_and_remove_container "${image_name}"
 
-##3 Test plugins installation with tar.bz2
-#echo '-> 3 Test plugins installation with tar.bz2'
-#image_name=glpi_3
-#docker run $docker_run_options --name "${image_name}" --env='GLPI_INSTALL_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.2%2B1.0/glpi-fusioninventory-9.2.1.0.tar.bz2' "${image}"
-#wait_for_string_in_container_logs "${image_name}" 'Starting up...'
-#if ! docker exec "${image_name}" test -d plugins/fusioninventory; then
-#  docker logs "${image_name}"
-#  false
-#fi
-#stop_and_remove_container "${image_name}"
-#
-#
-##4 Test plugins installation with tar.gz
-#echo '-> 4 Test plugins installation with tar.gz'
-#image_name=glpi_4
-#docker run $docker_run_options --name "${image_name}" --env='GLPI_INSTALL_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.1%2B1.1/fusioninventory-for-glpi_9.1.1.1.tar.gz' "${image}"
-#wait_for_string_in_container_logs "${image_name}" 'Starting up...'
-##test
-#if ! docker exec "${image_name}" test -d plugins/fusioninventory; then
-#  docker logs "${image_name}"
-#  false
-#fi
-#stop_and_remove_container "${image_name}"
-#
-#
-##5 Test plugins installation with old variable
-#echo '-> 5 Test plugins installation with old variable'
-#image_name=glpi_5
-#docker run $docker_run_options --name "${image_name}" --env='GLPI_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.1%2B1.1/fusioninventory-for-glpi_9.1.1.1.tar.gz' "${image}"
-#wait_for_string_in_container_logs "${image_name}" 'Starting up...'
-##test
-#if ! docker exec "${image_name}" test -d plugins/fusioninventory; then
-#  docker logs "${image_name}"
-#  false
-#fi
-#stop_and_remove_container "${image_name}"
-#
-#
-##6 Test web access
-#echo '-> 6 Test web access'
-#image_name=glpi_6
-#docker run $docker_run_options --name "${image_name}" --publish 8000:80 "${image}"
-#wait_for_string_in_container_logs "${image_name}" 'nginx entered RUNNING state'
-#sleep 4
-##test
-#if ! curl -v http://localhost:8000 2>&1 | grep --quiet 'install/install.php'; then
-#  docker logs "${image_name}"
-#  false
-#fi
-#stop_and_remove_container "${image_name}"
+
+#3 Test plugins installation with tar.gz
+echo '-> 3 Test plugins installation with tar.gz'
+image_name=glpi_3
+docker run $docker_run_options --name "${image_name}" --env='GLPI_INSTALL_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.3%2B1.2/fusioninventory-9.3+1.2.tar.gz' "${image_building_name}"
+wait_for_string_in_container_logs "${image_name}" 'Starting up...'
+#test
+if ! docker exec "${image_name}" test -d plugins/fusioninventory; then
+  docker logs "${image_name}"
+  false
+fi
+stop_and_remove_container "${image_name}"
+
+
+#4 Test plugins installation with old variable
+echo '-> 4 Test plugins installation with old variable'
+image_name=glpi_4
+docker run $docker_run_options --name "${image_name}" --env='GLPI_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9  .3%2B1.2/fusioninventory-9.3+1.2.tar.gz' "${image_building_name}"
+wait_for_string_in_container_logs "${image_name}" 'Starting up...'
+#test
+if ! docker exec "${image_name}" test -d plugins/fusioninventory; then
+  docker logs "${image_name}"
+  false
+fi
+stop_and_remove_container "${image_name}"
+
+
+#5 Test web access
+echo '-> 5 Test web access'
+image_name=glpi_5
+docker run $docker_run_options --name "${image_name}" --publish 8000:80 "${image_building_name}"
+wait_for_string_in_container_logs "${image_name}" 'nginx entered RUNNING state'
+sleep 5
+if ! curl -v http://localhost:8000 2>&1 | grep --quiet 'install/install.php'; then
+  docker logs "${image_name}"
+  false
+fi
+stop_and_remove_container "${image_name}"
