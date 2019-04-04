922009208b167d95e0224877d3feb61686d49f74
Michał Panasiewicz <wolvverinepld@gmail.com>
2019-04-04 17:24:11 +0200

    - merge with turgon37 repo version 3.0.1

--- tests.sh
+++ tests.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
+
+## Global settings
+# image name
+DOCKER_IMAGE="${DOCKER_REPO:-glpi}"
+
+## Initialization
 set -e
 set -x
 
-## Initialization
-image_building_name=`cat ${PWD}/_image_build`
+if [ -n ${IMAGE_VARIANT} ]; then
+  image_building_name="${DOCKER_IMAGE}:building_${IMAGE_VARIANT}"
+  image_tags_prefix="${IMAGE_VARIANT}-"
+  echo "-> set image variant '${IMAGE_VARIANT}' for build"
+else
+  image_building_name="${DOCKER_IMAGE}:building"
+fi
 docker_run_options='--detach'
+echo "-> use image name '${image_building_name}' for tests"
+
 
 ## Prepare
 if [[ -z $(which container-structure-test 2>/dev/null) ]]; then
   echo "Retrieving structure-test binary...."
-  if [[ "$TRAVIS_OS_NAME" != 'linux' ]]; then
+  if [[ -n "${TRAVIS_OS_NAME}" && "$TRAVIS_OS_NAME" != 'linux' ]]; then
     echo "container-structure-test only released for Linux at this time."
     echo "To run on OSX, clone the repository and build using 'make'."
     exit 1
@@ -22,14 +35,40 @@ fi
 
 # Download tools shim.
 if [[ ! -f _tools.sh ]]; then
-  curl -L -o ${PWD}/_tools.sh https://gist.github.com/Wolvverine/cd03f29d68600c8850aeed8fe2b3d464/raw \
-  && chmod +x ${PWD}/_tools.sh
+  curl -L -o ${PWD}/_tools.sh https://gist.github.com/Wolvverine/cd03f29d68600c8850aeed8fe2b3d464/raw
+
 fi
 source ${PWD}/_tools.sh
+
 
 ## Test
 container-structure-test \
     test --image "${image_building_name}" --config ./tests.yml
+
+## Ensure that required php extensions are installed
+extensions=`docker run --rm "${image_building_name}" php -m`
+for ext in apcu \
+           ctype \
+           curl \
+           dom \
+           gd \
+           imap \
+           json \
+           ldap \
+           mysqli \
+           openssl \
+           opcache \
+           soap \
+           xml \
+           xmlreader \
+           xmlrpc \
+           zlib; do
+  if ! echo "${extensions}" | grep -qi $ext; then
+    echo "missing PHP extension '$ext'" 1>&2
+    exit 1
+  fi
+done
+
 
 #2 Test plugins installation with tar.bz2
 echo '-> 2 Test plugins installation with tar.bz2'
@@ -48,19 +87,6 @@ stop_and_remove_container "${image_name}"
 echo '-> 3 Test plugins installation with tar.gz'
 image_name=glpi_3
 docker run $docker_run_options --name "${image_name}" --env='GLPI_INSTALL_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.3%2B1.2/fusioninventory-9.3+1.2.tar.gz' "${image_building_name}"
-wait_for_string_in_container_logs "${image_name}" 'Starting up...'
-# test
-if ! docker exec "${image_name}" test -d plugins/fusioninventory; then
-  docker logs "${image_name}"
-  false
-fi
-stop_and_remove_container "${image_name}"
-
-
-#4 Test plugins installation with old variable
-echo '-> 4 Test plugins installation with old variable'
-image_name=glpi_4
-docker run $docker_run_options --name "${image_name}" --env='GLPI_PLUGINS=fusioninventory|https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.3%2B1.2/fusioninventory-9.3+1.2.tar.gz' "${image_building_name}"
 wait_for_string_in_container_logs "${image_name}" 'Starting up...'
 # test
 if ! docker exec "${image_name}" test -d plugins/fusioninventory; then
